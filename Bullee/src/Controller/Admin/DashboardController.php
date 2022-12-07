7b56e458d236958b19d185a841d8cff1451616b5
BoBanard <117775290+VChiofalo@users.noreply.github.com>
2022-12-07 16:26:43 +0100

    Backup-16.26

--- Bullee/src/Controller/Admin/DashboardController.php
+++ Bullee/src/Controller/Admin/DashboardController.php
@@ -2,23 +2,29 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Articles;
+use App\Entity\User;
+use App\Entity\Answers;
+use App\Entity\Questions;
+use App\Entity\Results;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ArticlesCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -41,6 +47,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Articles', 'fas fa-list', Articles::class);
+        yield MenuItem::linkToCrud('Questions', 'fas fa-list', Questions::class);
+        yield MenuItem::linkToCrud('Answers', 'fas fa-list', Answers::class);
+        yield MenuItem::linkToCrud('Results', 'fas fa-list', Results::class);
     }
 }
