176ead6d231ad0c9cdfe5ffb9770206d8f64f598
Neauetmie <112520059+Neauetmie@users.noreply.github.com>
2022-12-08 15:58:52 +0100

    noemie-28

--- Bullee/public/assets/js/index.js
+++ Bullee/public/assets/js/index.js
@@ -1,3 +1,15 @@
-let qValues = document.querySelectorAll('#quest');
+let qValues = document.querySelectorAll('.answ');
+let dataValue = document.querySelector('#data');
 
-let qId = document.querySelectorAll('#answ');
+// Boucle ajout evenement lié au clic
+for (answer of qValues){
+    answer.addEventListener('click', addClick)
+};
+
+// Fonction pour recuperer les ID des questions et les envoiyer dans l'imput afin de pouvoir les recuperer en php.
+function addClick(e){
+    qValues = e.currentTarget;
+    qValues = qValues.getAttribute('data-id');
+    dataValue.value = qValues;
+    console.log(dataValue.value);
+};
--- Bullee/src/Controller/QuizController.php
+++ Bullee/src/Controller/QuizController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Questions;
+use App\Entity\Answers;
+use App\Repository\QuestionsRepository;
+use App\Repository\AnswersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuizController extends AbstractController
 {
-    #[Route('/quiz', name: 'app_quiz')]
-    public function index(): Response
+    //define Route
+    #[Route('/quiz/{questions_id}', name: 'app_quiz_question')]
+    public function question($questions_id, QuestionsRepository $questionsRepository , RequestStack $requestStack): Response
     {
-        return $this->render('quiz/homequiz.html.twig', [
-            'controller_name' => 'QuizController',
+        // get answers by question ID
+       $question = $questionsRepository -> findOneById($questions_id);
+
+       //initiate the session for each question 
+       return $this->render('quiz/index.html.twig', [
+            'question' => $question
         ]);
+
     }
 }
