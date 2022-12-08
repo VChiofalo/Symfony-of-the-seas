b553749213f8311cb8afcd9a3dc8954135514482
dautzenbergv <113978293+dautzenbergv@users.noreply.github.com>
2022-12-08 17:27:22 +0100

    quizquestionsokvirginieid28

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -19,6 +19,10 @@ class QuizController extends AbstractController
     #[Route('/quiz/{questions_id}', name: 'app_quiz_question')]
     public function question($questions_id, QuestionsRepository $questionsRepository , RequestStack $requestStack): Response
     {
+        //pour vider la teableau et recommencer le test
+        //$session = $requestStack->getSession();
+        //$session->set('question', []);
+
         // get answers by question ID
        $question = $questionsRepository -> findOneById($questions_id);
 
@@ -38,31 +42,39 @@ class QuizController extends AbstractController
         //retry the result of the previous page
         $session = $requestStack->getSession();
 
-        dd($session);
+        //dd($session);
         $tab = $session->get('question');
         /// create tab to store question and answers values
         if($tab == null){
             $tab = [];
         }
-        //push new values in the array
-        array_push($tab, [$request['id_question'], $request['id_answer']]);
 
-        dd($tab);
+        //dd();
+        //push new values in the array
+        array_push($tab, [$request -> request->get('id_question'), $request -> request->get('id_answer')]);
+
+        //dd($tab);
         $session->set('question', $tab);
 
         // allow to return to next question of id isn't 10, otherwise, go next 
-        $question = $questionsRepository -> findOneById($questions_id); 
+        //$question = $questionsRepository -> findOneById($questions_id); 
 
         //if there is less than 10 questions, return to the next question
 
-        if(count($tab <10)){
-            $this->redirect('quiz/save', [
-                'question' => (($questions_id)+1)
+        if(count($tab) <10){
+
+            $c = count($tab)+1;
+        
+
+            return $this->redirectToRoute('app_quiz_question', [
+                'questions_id' => $c
             ]);
             }
             //otherwise got to the result
             else {
-                $this->redirect('quiz/result');
+                return $this->redirectToRoute('app_quiz_result', [
+                    
+                ]);
             }  
 
     }
@@ -73,6 +85,8 @@ class QuizController extends AbstractController
      //unicorn because this is the "touchiest" function
      public function licorneGetResult (RequestStack $requestStack, Request $request): Response
      {
+
+
     
     // retry tab result
     //proceed to calculation
