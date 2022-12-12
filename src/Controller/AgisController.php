3d2efe4f9876c612f10cd271eb48f1b1c502e86e
BoBanard <117775290+VChiofalo@users.noreply.github.com>
2022-12-12 15:48:25 +0100

    Backup-15.48-12 dec

--- src/Controller/AgisController.php
+++ src/Controller/AgisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class AgisController extends AbstractController
 {
     #[Route('/agis', name: 'app_agis')]
-    public function index(): Response
+    public function index(ArticlesRepository $articlesRepository): Response
     {
+        
+        $articles = $articlesRepository -> findAll();
 
         return $this->render('agis/index.html.twig', [
             'controller_name' => 'AgisController',
+            'articles' => $articles
+        ]);
+    }
+
+    #[Route('/article/{slug}', name: 'app_agis_article')]
+    public function articles($slug, ArticlesRepository $articlesRepository): Response
+    {
+        
+        $article = $articlesRepository -> findOneBySlug($slug);
+
+        return $this->render('agis/index.html.twig', [
+            'article' => $article
         ]);
     }
 }
