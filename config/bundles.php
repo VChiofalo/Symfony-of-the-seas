6ad7dd7e9d571063d91fb843d0f3e716dc24a04e
BoBanard <117775290+VChiofalo@users.noreply.github.com>
2022-12-13 15:47:26 +0100

    backup-15.47-13 dec

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
 ];
