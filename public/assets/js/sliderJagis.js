aa622310fc3a2019c1358e2f9c9a00f2f1c903f7
Neauetmie <112520059+Neauetmie@users.noreply.github.com>
2022-12-16 16:47:53 +0100

    slide jagis a faire

---
+++ public/assets/js/sliderJagis.js
@@ -0,0 +1,33 @@
+//SLIDER A MODIFIER AVE LES CLASSES (REPRIS DE LA PAGE JE M'INFORME)
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    let btnPrev = document.querySelector('#temoignages-container .btn-prev');
+    let btnNext = document.querySelector('#temoignages-container .btn-next');
+    let delta = 0;
+    let counter = 0;
+    let step = 250;
+    let slider = document.querySelector('#temoignages');
+    let containerWidth = document.getElementById('temoignages-container').offsetWidth;
+    let nbSlide = slider.children.length;
+    let divid = Math.round(containerWidth / step);
+
+
+    if (!btnPrev || !btnNext) return;
+
+    btnPrev.addEventListener('click', (e) => {
+        if (counter > 0) {
+            delta = delta + step;
+            slider.style.transform = `translate(${delta}px, 0)`;
+            counter--;
+        }
+    });
+
+    btnNext.addEventListener('click', (e) => {
+        if (counter < (nbSlide - (divid - 2))) {
+            delta = delta - step;
+            slider.style.transform = `translate(${delta}px,0)`;
+            counter++;
+        }
+    });
+
+})
