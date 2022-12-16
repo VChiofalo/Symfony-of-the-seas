b6c13a1dbd23ba77ce51f357fe4bf2634ea10b29
Neauetmie <112520059+Neauetmie@users.noreply.github.com>
2022-12-16 13:36:27 +0100

    bouton inverse

--- public/assets/js/sliderTemoignages.js
+++ public/assets/js/sliderTemoignages.js
@@ -7,49 +7,30 @@ document.addEventListener('DOMContentLoaded', (e) => {
     let step = 250;
     let slider = document.querySelector('#temoignages');
     let containerWidth = document.getElementById('temoignages-container').offsetWidth;
-    let nbSlide= slider.children.length;
+    let nbSlide = slider.children.length;
+    let divid = Math.round(containerWidth / step);
 
-    let divid = Math.round(containerWidth  / step);
-    console.log(divid);
-    
+
     if (!btnPrev || !btnNext) return;
-    
-
-    let tailleMaDiv = document.getElementById('temoignages-container').style.height /* = tailleMaDiv+"px" */;
-    console.log(tailleMaDiv); 
 
     btnPrev.addEventListener('click', (e) => {
-        console.log(counter);
-        if(counter < (nbSlide - (divid - 2))) {
-            delta = delta - step;
-            slider.style.transform = `translate(${delta}px,0)`;
-            /*if (delta < 2500) {
-                delta = -700;
-            };    */     
-            console.log(delta);
-            counter++;
-        }
-
-       /*  if (btnleft < cardTemoign) {
-            btnleft = cardTemoign;            
-        } */
-       /*  console.log(btnLeft); */
-
-    });
-    
-    btnNext.addEventListener('click', (e) => {
-        if(counter > 0) {
+        if (counter > 0) {
             delta = delta + step;
             slider.style.transform = `translate(${delta}px, 0)`;
-            
-        
-            console.log(delta);
             counter--;
         }
     });
-    
+
+    btnNext.addEventListener('click', (e) => {
+        if (counter < (nbSlide - (divid - 2))) {
+            delta = delta - step;
+            slider.style.transform = `translate(${delta}px,0)`;
+            counter++;
+        }
+    });
+
 })
-let tailleMaDiv =  document.getElementById('temoignages-container').offfserHeight;
+
 
 
 
