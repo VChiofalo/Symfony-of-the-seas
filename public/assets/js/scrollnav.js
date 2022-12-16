770b049e99c0b85d735c62a2dbba8ffdbcdb7b81
dautzenbergv <113978293+dautzenbergv@users.noreply.github.com>
2022-12-16 16:16:47 +0100

    headerandinform

---
+++ public/assets/js/scrollnav.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.navbar');
+let height = 100;
+
+window.addEventListener('scroll', () => {
+    console.log (window.scrollY)
+    if(window.scrollY > height) {
+        nav.classList.add('scrollnav');
+    }
+    else{
+        nav.classList.remove('scrollnav');
+    }
+});
