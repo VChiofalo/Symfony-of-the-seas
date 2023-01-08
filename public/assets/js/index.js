31feb72ebd1ae1ab888b69718a5a2cddfeafbfe2
Neauetmie <noemie.sigrist@gmail.com>
2023-01-08 15:50:03 +0100

    quizz ok

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,12 +10,36 @@ for (answer of qValues){
     answer.addEventListener('click', addClick)
 };
 
+
+// Fonction qui change la couleur du background sur la réponse sélectionnée
+function changeColor(dataValue) {
+  console.log(dataValue.value);
+  let answers = document.querySelectorAll('.answ');
+  
+  answers.forEach(answer => {
+    {
+      value = answer.getAttribute("data-id");
+      if (dataValue.value === value)
+      {
+        let color = "background-color: #FF8EBF";
+        answer.setAttribute('style', color);
+      }
+      else{
+        let color = "background-color: #FFFFFF";
+        answer.setAttribute('style', color);
+      }
+      } 
+  })
+  // dataValue.setAttribute('background-color', '#FFFFFF');
+};
+
 // Fonction pour recuperer les ID des questions et les envoiyer dans l'input afin de pouvoir les recuperer en php.
  function addClick(e){
-     console.log('ok');
+    console.log('HELLO');
     qValues = e.currentTarget;
     qValues = qValues.getAttribute('data-id');
     dataValue.value = qValues;
+    changeColor(dataValue)
 };
 
 
