090549c5b22901af07f9236a4ff077d0bbb74108
Neauetmie <noemie.sigrist@gmail.com>
2023-01-23 09:52:31 +0100

    Update sliderTemoignages.js

--- public/assets/js/sliderTemoignages.js
+++ public/assets/js/sliderTemoignages.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     let btnNext = document.querySelector('.btn-next');
     let delta = 0;
     let counter = 0;
+    // permet de deplacer de 250px
     let step = 250;
     let slider = document.querySelector('#temoignages');
     let containerWidth = document.getElementById('temoignages-container').offsetWidth;
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 
     if (!btnPrev || !btnNext) return;
-
+//ajout de l'evenement click + defilement vers la gauche de 250px
     btnPrev.addEventListener('click', (e) => {
         if (counter > 0) {
             delta = delta + step;
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
             counter--;
         }
     });
-
+//Defilement vers la droite + blocage des cards a 2 temoignages sur le slider
     btnNext.addEventListener('click', (e) => {
         if (counter < (nbSlide - (divid - 2))) {
             delta = delta - step;
